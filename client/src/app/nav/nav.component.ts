192d6c84ecaec0f0b8ab96a48ef60a43c480e805
chris young <christopher.young@revature.net>
2021-08-13 16:51:23 -0400

    dropdown menu

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -9,11 +9,31 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {}
+  loggedIn: boolean = false;
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
+
   }
   login(){
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    });
+  }
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
+  }
+
+  getCurrentUser(){
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user; //turn into bool
+    }, error => {
+      console.log(error);
+    })
   }
 }
