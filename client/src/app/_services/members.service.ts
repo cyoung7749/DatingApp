cc2e9d5ffb40356cc881821d494b4acd91f5de3d
chris young <christopher.young@revature.net>
2022-07-21 18:02:31 -0400

    pfp complete

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -7,42 +7,48 @@ import { Member } from '../_models/member';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-}//don't need
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token,
+  }),
+}; //don't need
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
-baseUrl = environment.apiUrl;
-members: Member[] = [];
+  baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-   getMembers() {
+  getMembers() {
     if (this.members.length > 0) return of(this.members);
     return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions).pipe(
-      map(members => {
+      map((members) => {
         this.members = members;
         return members;
       })
-    )
+    );
   }
   getMember(username: string) {
-    const member = this.members.find(x => x.username === username);
+    const member = this.members.find((x) => x.username === username);
     if (member != undefined) return of(member);
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions)
-  } 
+    return this.http.get<Member>(
+      this.baseUrl + 'users/' + username,
+      httpOptions
+    );
+  }
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
         const index = this.members.indexOf(member);
         this.members[index] = member;
       })
-    )
+    );
   }
   setMainPhoto(photoId: number) {
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {})
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -5,34 +5,58 @@ import { User } from 'src/app/_models/user';
 import { Member } from 'src/app/_models/member';
 import { AccountService } from 'src/app/_services/account.service';
 import { environment } from 'src/environments/environment';
+import { MembersService } from 'src/app/_services/members.service';
+import { Photo } from 'src/app/_models/photo';
 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
-  styleUrls: ['./photo-editor.component.css']
+  styleUrls: ['./photo-editor.component.css'],
 })
-  export class PhotoEditorComponent implements OnInit {
+export class PhotoEditorComponent implements OnInit {
+  @Input() member: Member;
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  hasAnotherDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: User;
 
-@Input() member:Member;
-uploader: FileUploader;
-hasBaseDropZoneOver = false;
-hasAnotherDropZoneOver = false;
-baseUrl = environment.apiUrl;
-user: User;
-
-  constructor(private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
-   }
+  constructor(
+    private accountService: AccountService,
+    private memberService: MembersService
+  ) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
+  }
 
   ngOnInit(): void {
     this.initializeUploader();
   }
 
-  fileOverBase(e: any){
+  fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
   }
-  fileOverAnother(e: any){
+  fileOverAnother(e: any) {
     this.hasAnotherDropZoneOver = e;
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo.id).subscribe(() => {
+      this.user.photoUrl = photo.url;
+      this.accountService.setCurrentUser(this.user);
+      this.member.photoUrl = photo.url;
+      this.member.photos.forEach((p) => {
+        if (p.isMain) p.isMain = false;
+        if (p.id == photo.id) p.isMain = true;
+      });
+    });
+  }
+
+  deleteMainPhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter((x) => x.id !== photoId);
+    });
   }
   initializeUploader() {
     this.uploader = new FileUploader({
@@ -42,18 +66,18 @@ user: User;
       allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
-      maxFileSize: 10 * 1024 * 1024
+      maxFileSize: 10 * 1024 * 1024,
     });
 
     this.uploader.onAfterAddingFile = (file) => {
       file.withCredentials = false;
-    }
+    };
 
     this.uploader.onSuccessItem = (item, response, status, header) => {
       if (response) {
         const photo = JSON.parse(response);
         this.member.photos.push(photo);
       }
-    }
+    };
   }
 }
